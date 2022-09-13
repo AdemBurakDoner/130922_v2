c0c3dda4d9727798a1a53d6044db89900c556a25
KOCYASA\ademburak.doner <ademburak.doner@kocyasa.com>
2022-09-13 16:36:34 +0300

    env update

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/api/hello', async (req, res) => {
     res.send('Hello World')
 })
 
+app.get('/api/env', async (req, res) => {
+
+    const jsonText = JSON.stringify(process.env)
+    const connStr = process.env.CUSTOMCONNSTR_connStr || 'connection string'
+
+    res.send(connStr)
+})
+
 app.get('/api/merhaba', (req, res) => {
 
     res.send('Merhaba Dünya')
